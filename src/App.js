640bd330f463e03f3cbde1e53096d36c993c0fcc
James Schnebly <jameschnebly@gmail.com>
2020-03-21 15:08:41 -0700

    Formatting shop page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route component={HomePage} exact path='/' />
+        <Route component={ShopPage} exact path='/shop' />
       </Switch>
     </div>
   );
