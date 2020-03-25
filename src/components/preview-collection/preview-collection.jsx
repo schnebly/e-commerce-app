9bf0117a82fa7a196460d949c540b947a3c068eb
James Schnebly <jameschnebly@gmail.com>
2020-03-25 13:08:36 -0700

    Added shop page and collection preview

--- src/components/preview-collection/preview-collection.jsx
+++ src/components/preview-collection/preview-collection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item';
 
 const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
@@ -9,8 +10,8 @@ const CollectionPreview = ({title, items}) => (
             {
                 items
                 .filter((item, idx) => idx < 4)
-                .map(item => (
-                    <div key={item.id} >{item.name}</div>
+                .map(({id, ...itemProps}) => (
+                    <CollectionItem key={id} {...itemProps} />
                 ))
             }
         </div>
