e69a1e58015e50de1cde963d1cc8f67472d0162a
James Schnebly <jameschnebly@gmail.com>
2020-07-06 21:02:08 -0700

    Collection routing and redux selectors

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collectionsOverview/collectionsOverview';
+import CollectionPage from '../collection/collection';
 
-
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
 
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 );
 
